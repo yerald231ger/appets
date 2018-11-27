4516797482437e28d391735ebef9a86b55059487
Gerardo Sánchez Hernández <yerald231ger@live.com>
2018-11-26 18:57:13 -0600

    agregar migracion de fecha de nacimiento

--- Data/Migrations/20181127005525_Agregar Fecha Nacimiento.Designer.cs
+++ Data/Migrations/20181127005525_Agregar Fecha Nacimiento.Designer.cs
@@ -4,14 +4,16 @@ using ApPets.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ApPets.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181127005525_Agregar Fecha Nacimiento")]
+    partial class AgregarFechaNacimiento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20181127005525_Agregar Fecha Nacimiento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApPets.Migrations
+{
+    public partial class AgregarFechaNacimiento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthdate",
+                table: "tblUser",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthdate",
+                table: "tblUser");
+        }
+    }
+}
